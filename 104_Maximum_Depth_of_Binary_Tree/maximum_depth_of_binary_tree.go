48919a01e4f188214cee4e480e7eaf6d27ea3146
Artyom Shirshov <artyomsh01@yandex.ru>
2023-09-11 13:33:11 +0300

    max depth solved

--- 104_Maximum_Depth_of_Binary_Tree/maximum_depth_of_binary_tree.go
+++ 104_Maximum_Depth_of_Binary_Tree/maximum_depth_of_binary_tree.go
@@ -5,12 +5,81 @@ type TreeNode struct {
 	Left *TreeNode
 	Right *TreeNode
 }
-func maxDepth(root *TreeNode) int {
-	length := 0
-	if root != nil {
-		length = maxDepth(root.Left)
-		length = maxDepth(root.Right)
+
+func max(first, second int) int {
+	if first >= second {
+		return first
 	}
-   
-	return length + 1
+	return second
+}
+
+func maxDepthRecursive(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	
+	return 1 + max(maxDepthRecursive(root.Left), maxDepthRecursive(root.Right))
+}
+
+func maxDepthBFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var queue []*TreeNode
+
+	level := 0
+	queue = append(queue, root)
+	for len(queue) != 0 {
+		for _, node := range queue {
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+
+			queue = queue[1:]
+		}
+
+		level++
+	}
+
+	return level
+}
+
+func maxDepthIDFS(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	type TreeNodeWithDepth struct {
+		*TreeNode
+		depth int
+	}
+
+	var stack []*TreeNodeWithDepth
+	maxdepth := 0
+
+	stack = append(stack, &TreeNodeWithDepth{root, 1})
+
+	for len(stack) != 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if maxdepth < node.depth {
+			maxdepth = node.depth
+		}
+
+		if node.Right != nil {
+			stack = append(stack, &TreeNodeWithDepth{node.Right, node.depth + 1})
+		}
+
+		if node.Left != nil {
+			stack = append(stack, &TreeNodeWithDepth{node.Left, node.depth + 1})
+		}
+	}
+
+	return maxdepth
 }
